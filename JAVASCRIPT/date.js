8217f331b08ddc3b93e1f8026f361996055bef95
NANDANI KUMARI <srivastavanandani190@gamil.com>
2025-12-14 22:01:25 +0530

    dates

---
+++ JAVASCRIPT/date.js
@@ -0,0 +1,20 @@
+// const now=new Date();
+// console.log(now);//ust
+// console.log(now.toString());//system time
+// console.log(now.toISOString());//India standard time
+// console.log(now.toLocaleString());
+// console.log(now.getDate());
+// console.log(now.getDay());//1 based
+// console.log(now.getMonth());//0 based
+// console.log(now.getFullYear());
+// console.log(now.getHours());
+// console.log(now.getSeconds());
+const newnow=new Date(2005,1,31,6,30,20,22);
+console.log(newnow);
+const now=Date.now();
+// const dates=new Date(123545912);
+// console.log(dates.toString());
+const dates=new Date(0);
+console.log(dates.toString());
+
+
