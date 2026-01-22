c9e4c2d160b6c62d2e22967fabd1ba1fb1604101
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-01-22 18:20:10 +0530

    new

--- JAVASCRIPT/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-console.log("Start");
-// setTimeout(() => {
-//   console.log("Timeout finished1");
-// }, 2000); //asynchronous operation
-// setTimeout(() => {
-//   console.log("Timeout finished2");
-// }, 3000); //setTimeout is a web API provided by the browser(environment) to handle asynchronous operations.
-// console.log("End");
