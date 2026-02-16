8108776577caa2b8aefd47dbc494f5df8507279a
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-02-16 09:27:51 +0530

    array

---
+++ JS/array.js
@@ -0,0 +1,12 @@
+//array->it is defined as a collection of disimilar data types.it is used to store multiple values in a single variable.
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits[0]); // apple
+console.log(fruits.length); // 3
+fruits.push("grape");
+console.log(fruits); // ["apple", "banana", "orange", "grape"]
+fruits.pop();
+console.log(fruits); // ["apple", "banana", "orange"]
+let arr1=["java",1,"hello"];
+console.log(arr1[0]); // java
+console.log(arr1[1]); // 1
+console.log(arr1[2]); // hello
