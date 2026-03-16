80820460576652588f98c7d6e2dead8bd44de8e5
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-03-16 09:38:20 +0530

    reactjs

---
+++ REACT/basic/src/App1.jsx
@@ -0,0 +1,9 @@
+ function APP1() {//captial denotes component and small indicates function
+  return (
+    <div> 
+    <h1> Hello World </h1 >
+    <h2>java script</h2>
+    </div>
+  )
+}
+export default APP1;// this is used to export the component to be used in other files like index.js where we render the component to the DOM.
