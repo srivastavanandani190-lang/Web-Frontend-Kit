8e689f6a34565e17c9d159bc04bbdec7caf14f70
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-03-23 09:30:41 +0530

    react

--- REACT/basic/src/Book.jsx
+++ REACT/basic/src/Book.jsx
@@ -1,7 +1,24 @@
+// import Detail from "./Detail.jsx";
 function Book(){
-    return(
+ 
+    const Book1={id:1,
+        Name:"c",
+        price:5000
+    }
+// ,{
+//     id:2,
+//     Name:"hello",
+//     price:2000
+// }]
+    
+   return(
         <div>
             <h1>Book Component</h1>
+            {/* <Detail data={Book1
+            }/> */}
+            <h2 >book name is {Book1.Name}</h2>
+            <h3>book id is {Book1.id}</h3>
+            <h4>book price is {Book1.price}</h4>
         </div>
     )
 }
---
+++ REACT/basic/src/Detail.jsx
@@ -0,0 +1,6 @@
+function Detail(){
+return (
+    <h1>Detail Component</h1>
+)
+}
+export default Detail;
