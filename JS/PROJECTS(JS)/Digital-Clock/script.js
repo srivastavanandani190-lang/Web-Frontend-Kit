0301673f4b91a021a6b2c82e405fd1f52ab5454b
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-07-02 19:04:26 +0530

    clock

--- JS/PROJECTS(JS)/Digital-Clock/script.js
+++ JS/PROJECTS(JS)/Digital-Clock/script.js
@@ -1,6 +1,6 @@
 const clock=document.getElementById("clock");
 
-
+//setinterval is a function that will run every 1000 milliseconds (1 second) and update the clock with the current time.
 setInterval(function(){
 let date=new Date();
 console.log(date.toLocaleTimeString());
