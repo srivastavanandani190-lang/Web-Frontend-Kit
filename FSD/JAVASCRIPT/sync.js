141adeda753b2949452126713bde854165b8453b
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-08-25 20:35:49 +0530

    javascript

---
+++ FSD/JAVASCRIPT/sync.js
@@ -0,0 +1,9 @@
+console.log("task 1");
+for(let i=1; i<=1000000000; i++) {  
+console.log(i);
+}
+console.log("task 2");
+console.log("task 3");
+console.log("hello ABES");
+//sync->you can't jump to another task until the current task is completed
+        //async->you can jump to another task until the current task is completed(can process but don't wait for the current task to complete)
